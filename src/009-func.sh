eadc0b43013e136de4320b2f95a86691b720a4d9
afeiship <1290657123@qq.com>
2019-07-15 11:41:25 +0800

    add: func

---
+++ src/009-func.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# write a function
+fresh(){
+    # t stores $1 argument passed to fresh()
+    t=$1
+    echo "fresh(): \$0 is $0"
+    echo "fresh(): \$1 is $1"
+    echo "fresh(): \$t is $t"
+    echo "fresh(): total args passed to me $#"
+    echo "fresh(): all args (\$@) passed to me -\"$@\""
+    echo "fresh(): all args (\$*) passed to me -\"$*\""
+}
